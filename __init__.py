dbc36d2ff456ba14ba66fd5cd2f985b0fb7b050d
ly15927086342 <952700304@qq.com>
2020-10-28 00:48:43 +0800

    feat: 新增重爬方法

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 	field = ['title','price','unit','type','area','direction','floor','decoration','community','subway','address','bdlat','bdlng','description']
 	
 	# 爬取指定区域的页面
-	app = chuzuSpider(region,field,THREAD_MAX,DICT_PATH)
+	app = chuzuSpider(regions = region,fields = field,thread_num = THREAD_MAX,dict_path = DICT_PATH)
 	app.run()
 	
 	# 对爬取失败的文件重爬，和app.run()不能顺序执行，因为两个函数都是异步函数
