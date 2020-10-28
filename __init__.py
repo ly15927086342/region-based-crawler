0900c0b6ad1479940bd1d9c421352a443d4202d8
ly15927086342 <952700304@qq.com>
2020-10-28 12:43:54 +0800

    feat: 封装不同子类爬虫代码块

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 #coding = 'utf-8'
 
 from chuzuSpider import *
+from ershoufangSpider import *
 import csv
 
 # 线程数
@@ -9,8 +10,7 @@ THREAD_MAX = 3
 # 文件夹相对路径
 DICT_PATH = './result'
 
-# 主程序入口
-if __name__ == '__main__':
+def chuzuSpide():
 	region = ['xinzhouqu','hannan','hongshan','wuchang','jiangan','hanyang','jiangxia','jianghan','dongxihu','qiaokou','huangpo','whtkfq','whqingshanqu','caidian']
 	field = ['title','price','unit','type','area','direction','floor','decoration','community','subway','address','bdlat','bdlng','description']
 	
@@ -21,3 +21,19 @@ if __name__ == '__main__':
 	# 对爬取失败的文件重爬，和app.run()不能顺序执行，因为两个函数都是异步函数
 	# appR = chuzuSpider([],field,THREAD_MAX,DICT_PATH)
 	# appR.reSpideFailLinks()
+
+def ershoufangSpide():
+	region = ['xinzhouqu','hannan','hongshan','wuchang','jiangan','hanyang','jiangxia','jianghan','dongxihu','qiaokou','huangpo','whtkfq','whqingshanqu','caidian']
+	field = ['title','price','unit','type','area','direction','floor','decoration','community','subway','address','bdlat','bdlng','description']
+	
+	# 爬取指定区域的页面
+	app = ershoufangSpider(regions = region,fields = field,thread_num = THREAD_MAX,dict_path = DICT_PATH)
+	app.run()
+	
+	# 对爬取失败的文件重爬，和app.run()不能顺序执行，因为两个函数都是异步函数
+	# appR = chuzuSpider([],field,THREAD_MAX,DICT_PATH)
+	# appR.reSpideFailLinks()
+
+# 主程序入口
+if __name__ == '__main__':
+	ershoufangSpide()
