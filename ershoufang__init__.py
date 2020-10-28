35d9de00627559c5d13e7f478bf63bfea01d3e6c
ly15927086342 <952700304@qq.com>
2020-10-29 01:14:54 +0800

    style: 删除无用代码

--- ershoufang__init__.py
+++ ershoufang__init__.py
@@ -25,9 +25,4 @@ if __name__ == '__main__':
 	app.run()
 	
 	# 对爬取失败的文件重爬，和app.run()不能顺序执行，因为两个函数都是异步函数
-	appR = chuzuSpider(regions = [],
-		fields = field,
-		thread_num = THREAD_MAX,
-		dict_path = DICT_PATH,
-		timespan = TIMESPAN)
-	appR.reSpideFailLinks()
+	# app.reSpideFailLinks()
